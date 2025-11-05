c402ec32d46d5d0ae8fafc838ad1210e749e0a9f
Udson Silva <udson2112@gmail.com>
2025-11-05 23:47:58 +0000

    Código Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -29,18 +29,26 @@ int main() {
     }
 
     // Declaração e inicialização dos vetores dos navios
-    // Navio: {Tamanho, 0=Horizontal/1=Vertical, Início_Linha[0..9], Início_Coluna[0..9]}
-    int navios[2][4] = {
-        {3, 0, 1, 5},
-        {3, 1, 3, 5},
+    // Navio: {Tamanho, 0=Horizontal/1=Vertical/2=DiagonalCrescente/3=DiagonalDecrescente, Linha_Início[0..9], Coluna_Início[0..9]}
+    int navios[4][4] = {
+        {3, 0, 2, 4},
+        {3, 1, 3, 2},
+        {5, 3, 4, 4},
+        {3, 2, 7, 2},
+        
     };
 
 
     printf(GREEN"\n\n    .:: BATALHA NAVAL ::.\n\n"RESET);
     // Insere navios no tabuleiro
-    for(int i = 0; i < 4; i++) {    // Percorre o vetor de navios
-        if(navios[i][1] == 0) {     // Seleciona navios na horizontal
-            if((TAB_H - navios[i][3] - navios[i][0]) >= 0) { // Verifica se o navio cabe no tabuleiro
+    for(int i = 0; i < sizeof(navios) / sizeof(navios[0]); i++) {    // Percorre o vetor de navios
+        switch (navios[i][1])
+        {
+        case 0: // Seleciona navios na horizontal
+            if ((navios[i][2] >= 0) &&
+                (navios[i][3] >= 0) &&
+                (navios[i][3] < TAB_V) &&
+                (TAB_H - navios[i][3] - navios[i][0] >= 0)) { // Verifica se o navio cabe no tabuleiro
                 for(int j = 0; j < navios[i][0]; j++) {
                     if(tabuleiro[navios[i][2]][navios[i][3] + j][0] == 3) {    // Verifica conicidências entre navios
                         printf(RED"Erro! O navio %d coincide com outro navio na casa %c%02d.\n"RESET, i + 1, navios[i][3] + j + 65, navios[i][2] + 1);
@@ -56,8 +64,13 @@ int main() {
                 printf(RED"Erro! O navio %d ultrapassa os limites do tabuleiro.\n"RESET, i + 1);
                 erro = 1;
             }
-        } else {    // Seleciona navios na vertical
-            if((TAB_V - navios[i][2] - navios[i][0]) >= 0) { // Verifica se o navio cabe no tabuleiro
+            break;
+        
+        case 1: // Seleciona navios na vertical
+            if ((navios[i][2] >= 0) &&
+                (navios[i][3] >= 0) &&
+                (navios[i][2] < TAB_H) &&
+                (TAB_V - navios[i][2] - navios[i][0] >= 0)) { // Verifica se o navio cabe no tabuleiro
                 for(int j = 0; j < navios[i][0]; j++) {
                     if(tabuleiro[navios[i][2] + j][navios[i][3]][0] == 3) {    // Verifica conicidências entre navios
                         printf(RED"Erro! O navio %d coincide com outro navio na casa %c%02d.\n"RESET, i + 1, navios[i][3] + 65, navios[i][2] + j + 1);
@@ -72,6 +85,51 @@ int main() {
                 printf(RED"Erro! O navio %d ultrapassa os limites do tabuleiro.\n"RESET, i + 1);
                 erro = 1;
             }
+            break;
+        
+        case 2: // Seleciona navios na diagonal crescente
+            if ((navios[i][3] >= 0) &&
+                (navios[i][2] < TAB_H) &&
+                (navios[i][2] - navios[i][0] - 1 >= 0) &&
+                (navios[i][3] + navios[i][0] - 1 < TAB_V)) { // Verifica se o navio cabe no tabuleiro
+                for(int j = 0; j < navios[i][0]; j++) {
+                    if(tabuleiro[navios[i][2] + j][navios[i][3] - j][0] == 3) {    // Verifica conicidências entre navios
+                        printf(RED"Erro! O navio %d coincide com outro navio na casa %c%02d.\n"RESET, i + 1, navios[i][3] + j + 65, navios[i][2] + 1);
+                        tabuleiro[navios[i][2] + j][navios[i][3] - j][1] = 31;
+                    } else {
+                        tabuleiro[navios[i][2] + j][navios[i][3] - j][0] = 3;
+                        tabuleiro[navios[i][2] + j][navios[i][3] - j][1] = 34;
+                        
+                    }
+                }
+            }
+            else {
+                printf(RED"Erro! O navio %d ultrapassa os limites do tabuleiro.\n"RESET, i + 1);
+                erro = 1;
+            }
+            break;
+        
+        case 3: // Seleciona navios na diagonal decrecrescente
+            if ((navios[i][2] >= 0) &&
+                (navios[i][3] >= 0) &&
+                (navios[i][2] + navios[i][0] - 1 < TAB_H) &&
+                (navios[i][3] + navios[i][0] - 1 < TAB_V)) { // Verifica se o navio cabe no tabuleiro
+                for(int j = 0; j < navios[i][0]; j++) {
+                    if(tabuleiro[navios[i][2] + j][navios[i][3] + j][0] == 3) {    // Verifica conicidências entre navios
+                        printf(RED"Erro! O navio %d coincide com outro navio na casa %c%02d.\n"RESET, i + 1, navios[i][3] + j + 65, navios[i][2] + 1);
+                        tabuleiro[navios[i][2] + j][navios[i][3] + j][1] = 31;
+                    } else {
+                        tabuleiro[navios[i][2] + j][navios[i][3] + j][0] = 3;
+                        tabuleiro[navios[i][2] + j][navios[i][3] + j][1] = 34;
+                        
+                    }
+                }
+            }
+            else {
+                printf(RED"Erro! O navio %d ultrapassa os limites do tabuleiro.\n"RESET, i + 1);
+                erro = 1;
+            }
+            break;
         }
     }
 
